ee68aa539125d174b32ce4a4539b3241c71d7455
June Young Kang <jkang34@gmail.com>
2020-10-12 03:16:39 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,8 @@
 # don't forget to add: require 'pry'
 
 def snake_it_up(string)
-  counter == 0
-  n == 10
+  counter = 0
+  n = 10
   if string[0] == "s"
     while counter < n
     string[0] + string
