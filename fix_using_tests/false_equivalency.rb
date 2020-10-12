28589cd74aa72bcb1c47f63f8b57d1cbd909f8b2
June Young Kang <jkang34@gmail.com>
2020-10-12 03:03:12 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -14,10 +14,11 @@ end
 
 def selection(num)
   if num = 1
-    puts "YUM YUM MUNCH MUNCH MUNCH"
+    "YUM YUM MUNCH MUNCH MUNCH"
   else num = 2
-    puts "HAM HAM HAM IN MY TUMMY"
+    "HAM HAM HAM IN MY TUMMY"
   end
+  binding.pry
 end
 
 def runner
